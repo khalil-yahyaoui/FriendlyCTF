46a421200241fd3d32b61544e7c9db3959fbd029
khalil-yahyaoui <56357203+khalil-yahyaoui@users.noreply.github.com>
2021-10-01 18:25:09 +0100

    Create aes1.py

---
+++ AES1/aes1.py
@@ -0,0 +1,11 @@
+#AES is a symmetric-key block cipher , having the key and knowing the coorect mode you should be able to decrypt the ciphertext 
+#we know from task description : the key , the ciphertext and the mode used to encrypt
+
+from Crypto.Cipher import AES
+
+ciphertext = bytes.fromhex("5b5d13f1157ebee75756296793d8b998497c7849aac925473dd875871f845b0e")
+key = bytes.fromhex("4eb9b29b665a412894eef0692dda6735")
+
+#Creating an AES instance and specifying the key and the mode
+cipher = AES.new(key,AES.MODE_ECB)
+print(cipher.decrypt(ciphertext))
